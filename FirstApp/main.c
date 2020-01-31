d13894dedb78ea5b6c68708cedd2c36ff1cddacf
andrewbyun112 <nimbus112@naver.com>
2020-01-31 11:11:16 +0900

    2장 작업

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -1,9 +1,18 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif // _MSC_VER
+
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	printf("Hello world!");
-	// 여기는 주석입니다.
-	// This is a comment.asfh
+	printf("Hello world!\n");
+
+	// type here
+
+	system("pause");
+
 	return 0;
 }
---
+++ TemplateTest/main.c
@@ -0,0 +1,18 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif // _MSC_VER
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234567890\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
